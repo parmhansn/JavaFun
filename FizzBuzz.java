7941114751fa9752ed8d3ae9deb44748e9073b7b
Nadia Parmhans <nparmhans@gmail.com>
2019-01-09 11:19:01 -0500

    Added FizzBuzz

---
+++ FizzBuzz.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class FizzBuzz {
+    public void fizzBuzz(int number) {
+            if (number % 3 == 0 && number % 5 == 0){
+                System.out.println("FizzBuzz");
+            }
+            else if (number % 3 == 0){
+                System.out.println("Fizz");
+            }
+            else if (number % 5 == 0){
+                System.out.println("Buzz");
+            }
+            else{
+                System.out.println(number);
+            }
+        }
+    }
---
+++ FizzBuzzTest.java
@@ -0,0 +1,6 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz fB = new FizzBuzz(); //FizzBuzz is an instance of the FizzBuzz class in FizzBuzz.java! Anytime you wanan use methods in FizzBuzz it's being referred to as fB. newFizzBuzz means you're creating a new FizZBuzz object. 
+        fB.fizzBuzz(25);
+    }
+}
